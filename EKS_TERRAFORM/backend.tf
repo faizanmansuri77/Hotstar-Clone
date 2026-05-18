7dab9854ddf8616bf1bf3c42bf6fed60b35e30c9
Faizan Mansuri <faizanahmedmansuri@gmail.com>
2026-05-18 17:41:09 +0530

    Change S3 key for Terraform backend configuration

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "openai-chatbot-ui" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
+    key    = "Hotstar-EKS/terraform.tfstate"
     region = "ap-south-1"
   }
 }
